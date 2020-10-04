bb55c3e233a5b5eb0d7cc5a94f46601ac9092bec
princy1612 <70192372+princy1612@users.noreply.github.com>
2020-10-04 12:47:39 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -43,7 +43,7 @@ public class Hook extends BaseUtil{
 //        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
 //        base.Driver = new ChromeDriver();
 
-        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Web/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
